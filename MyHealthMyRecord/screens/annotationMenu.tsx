e13083be3ba10c21dd0e4f5d044b9c431ecea2d9
khushdip <65426916+khushdip@users.noreply.github.com>
2023-06-26 12:44:33 -0400

    is annotated button icons

--- MyHealthMyRecord/screens/annotationMenu.tsx
+++ MyHealthMyRecord/screens/annotationMenu.tsx
@@ -1,8 +1,8 @@
 /*import React from 'react';
 import type { PropsWithChildren } from 'react';*/
-import {ParamListBase, useNavigation, useRoute} from '@react-navigation/native';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import React, {useEffect, useRef, useState} from 'react';
+import { ParamListBase, useIsFocused, useNavigation, useRoute } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   NativeSyntheticEvent,
   SafeAreaView,
@@ -12,10 +12,13 @@ import {
   TextInputFocusEventData,
   View,
 } from 'react-native';
-import {Button, Icon, Input} from '@rneui/themed';
-import {VideoData, useObject, useRealm} from '../models/VideoData';
+import { Button, Icon, Input } from '@rneui/themed';
+import { VideoData, useObject, useRealm } from '../models/VideoData';
 
 const AnnotationMenu = () => {
+  //is the current screen focused, used for state changes between navigation
+  const isFocused = useIsFocused();
+
   const route: any = useRoute();
   const id = route.params?.id;
 
@@ -26,6 +29,11 @@ const AnnotationMenu = () => {
   const titleInput: any = useRef(null);
 
   const [title, setTitle] = React.useState(video.title);
+
+  const [keywordButtonColour, setKeywordButtonColour] = React.useState("#C7CBD1");
+  const [keywordButtonType, setKeywordButtonType] = React.useState("add-outline");
+  const [locationButtonColour, setLocationButtonColour] = React.useState("#C7CBD1");
+  const [locationButtonType, setLocationButtonType] = React.useState("add-outline");
 
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
@@ -38,20 +46,57 @@ const AnnotationMenu = () => {
     }
   };
 
+  const checkIfKeywordsAnnotated = () => {
+    let isAnnotated = false;
+    const keywords = video.keywords;
+    keywords.map((key: string) => {
+      if (JSON.parse(key).checked) isAnnotated = true;
+    })
+    return isAnnotated;
+  }
+
+  const checkIfLocationsAnnotated = () => {
+    let isAnnotated = false;
+    const locations = video.locations;
+    locations.map((loc: string) => {
+      if (JSON.parse(loc).checked) isAnnotated = true;
+    })
+    return isAnnotated;
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      if (checkIfKeywordsAnnotated()) {
+        setKeywordButtonColour("#1C3EAA");
+        setKeywordButtonType("checkmark-outline");
+      } else {
+        setKeywordButtonColour("#C7CBD1");
+        setKeywordButtonType("add-outline");
+      }
+      if (checkIfLocationsAnnotated()) {
+        setLocationButtonColour("#1C3EAA");
+        setLocationButtonType("checkmark-outline");
+      } else {
+        setLocationButtonColour("#C7CBD1");
+        setLocationButtonType("add-outline");
+      }
+    }
+  }, [keywordButtonColour, keywordButtonType, locationButtonColour, locationButtonType, isFocused]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Input
         ref={titleInput}
-        inputStyle={{fontSize: 35}}
+        inputStyle={{ fontSize: 35 }}
         //value={text}
         defaultValue={title}
         onChangeText={value => setTitle(value)}
         onSubmitEditing={() => updateVideoTitle()}
       />
-      <Text style={{fontSize: 24}}>
+      <Text style={{ fontSize: 24 }}>
         Select how you would like to start annotating your video:
       </Text>
-      <View style={{paddingTop: 45}}>
+      <View style={{ paddingTop: 45 }}>
         <View style={styles.selectionContainer}>
           {video.painScale.length == 0 ? (
             <Icon
@@ -75,49 +120,25 @@ const AnnotationMenu = () => {
           <Text style={styles.textStyle}>Adjust Painscale</Text>
         </View>
         <View style={styles.selectionContainer}>
-          {video.keywords.length == 0 ? (
-            <Icon
-              reverse
-              name="add-outline"
-              size={40}
-              type="ionicon"
-              color="#C7CBD1"
-              onPress={() => navigation.navigate('Keywords', {id})}
-            />
-          ) : (
-            <Icon
-              reverse
-              name="checkmark-outline"
-              size={40}
-              type="ionicon"
-              color="#1C3EAA"
-              onPress={() => navigation.navigate('Keywords', {id})}
-            />
-          )}
-
+          <Icon
+            reverse
+            name={keywordButtonType}
+            size={40}
+            type={"ionicon"}
+            color={keywordButtonColour}
+            onPress={() => navigation.navigate('Keywords', { id })}
+          />
           <Text style={styles.textStyle}>Add Keywords</Text>
         </View>
         <View style={styles.selectionContainer}>
-          {video.locations.length == 0 ? (
-            <Icon
-              reverse
-              name="add-outline"
-              size={40}
-              type="ionicon"
-              color="#C7CBD1"
-              onPress={() => navigation.navigate('Location', {id})}
-            />
-          ) : (
-            <Icon
-              reverse
-              name="checkmark-outline"
-              size={40}
-              type="ionicon"
-              color="#1C3EAA"
-              onPress={() => navigation.navigate('Location', {id})}
-            />
-          )}
-
+          <Icon
+            reverse
+            name={locationButtonType}
+            size={40}
+            type="ionicon"
+            color={locationButtonColour}
+            onPress={() => navigation.navigate('Location', { id })}
+          />
           <Text style={styles.textStyle}>Add Location</Text>
         </View>
         <View style={styles.selectionContainer}>
@@ -184,8 +205,8 @@ const AnnotationMenu = () => {
       </View>
 
       <Button
-        containerStyle={{paddingTop: 100}}
-        buttonStyle={{width: 220, height: 75, alignSelf: 'center'}}
+        containerStyle={{ paddingTop: 100 }}
+        buttonStyle={{ width: 220, height: 75, alignSelf: 'center' }}
         color="#1C3EAA"
         title="Review Markups"
         onPress={() =>
@@ -199,7 +220,7 @@ const AnnotationMenu = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {padding: 75},
+  container: { padding: 75 },
   textStyle: {
     alignSelf: 'center',
     fontSize: 24,
