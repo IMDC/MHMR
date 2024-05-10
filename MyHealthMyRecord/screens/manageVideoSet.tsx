7b73f03f7a0a8643d9f9b22c9952a8b65dc85a8f
varshigak <varshigak@gmail.com>
2024-05-10 11:47:16 -0400

    Display name of video set on manage video set page

--- MyHealthMyRecord/screens/manageVideoSet.tsx
+++ MyHealthMyRecord/screens/manageVideoSet.tsx
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  ScrollView,
-  ImageBackground,
-  TouchableOpacity
-} from 'react-native';
+import { StyleSheet, View, Text, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { Button, Icon } from '@rneui/themed';
 import RNFS from 'react-native-fs';
-import { useRealm, VideoData } from '../models/VideoData';
+import { useRealm, VideoData, VideoSet } from '../models/VideoData';
 import * as Styles from '../assets/util/styles';
 
 const ManageVideoSet = () => {
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
   const realm = useRealm();
   const [videos, setVideos] = useState<VideoData[]>([]);
+  const [videoSets, setVideoSets] = useState<VideoSet[]>([]);
   const MHMRfolderPath = RNFS.DocumentDirectoryPath + '/MHMR';
 
   useEffect(() => {
+    fetchVideoSets();
     fetchVideos();
   }, [realm]);
+
+  const fetchVideoSets = () => {
+    const sets = realm.objects<VideoSet>('VideoSet');
+    setVideoSets(Array.from(sets));
+  };
 
   const fetchVideos = () => {
     const videoSet = realm.objects<VideoData>('VideoData').filtered('isSelected == true');
@@ -41,24 +41,22 @@ const ManageVideoSet = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Manage Video Set</Text>
       <ScrollView>
+        {videoSets.map((set, index) => (
+          <View key={set._id.toString()} style={styles.setContainer}>
+            <Text style={styles.setName}>{set.name}</Text>
+          </View>
+        ))}
         {videos.map((video, index) => (
           <View key={video._id.toString()} style={styles.videoContainer}>
-            <ImageBackground
-              source={{ uri: `file://${MHMRfolderPath}/${video.filename}` }}
-              style={styles.thumbnail}
-            >
-              <TouchableOpacity onPress={() => navigation.navigate('VideoDetail', { videoId: video._id })}>
+            <TouchableOpacity onPress={() => navigation.navigate('VideoDetail', { videoId: video._id })}>
+              <ImageBackground source={{ uri: `file://${MHMRfolderPath}/${video.filename}` }} style={styles.thumbnail}>
                 <Icon name="play-circle" type="ionicon" size={50} color="#fff" />
-              </TouchableOpacity>
-            </ImageBackground>
+              </ImageBackground>
+            </TouchableOpacity>
             <View style={styles.infoContainer}>
               <Text style={styles.videoTitle}>{video.title}</Text>
               <View style={styles.actionContainer}>
-                <Button
-                  title="Remove"
-                  onPress={() => handleRemoveVideo(video)}
-                  color={Styles.MHMRBlue}
-                />
+                <Button title="Remove" onPress={() => handleRemoveVideo(video)} color={Styles.MHMRBlue} />
               </View>
             </View>
           </View>
@@ -79,10 +77,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20
   },
+  setContainer: {
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    marginBottom: 10
+  },
+  setName: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
   videoContainer: {
     flexDirection: 'row',
-    marginBottom: 15,
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 20
   },
   thumbnail: {
     width: 100,
@@ -94,17 +101,16 @@ const styles = StyleSheet.create({
     marginRight: 10
   },
   infoContainer: {
-    flex: 1,
-    justifyContent: 'space-between'
+    flex: 1
   },
   videoTitle: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginBottom: 10
   },
   actionContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 10
+    justifyContent: 'flex-end'
   }
 });
 
