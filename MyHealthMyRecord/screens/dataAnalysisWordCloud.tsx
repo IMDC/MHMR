3e0b7dfdda85972e9754448a2f7b709d1f189d0a
varshigak <varshigak@gmail.com>
2024-05-17 12:11:49 -0400

    Increase font size for words in word cloud

--- MyHealthMyRecord/screens/dataAnalysisWordCloud.tsx
+++ MyHealthMyRecord/screens/dataAnalysisWordCloud.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import {ParamListBase, useNavigation, useRoute} from '@react-navigation/native';
-import {Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ParamListBase, useNavigation, useRoute } from '@react-navigation/native';
+import { Text, View } from 'react-native';
 import WordCloud from 'rn-wordcloud';
 
 const DataAnalysisWordCloud = () => {
@@ -8,19 +8,19 @@ const DataAnalysisWordCloud = () => {
   const barData = route.params?.data;
   const [wordFrequency, setWordFrequency] = useState(barData.data);
   const data = [
-    {text: 'happy', value: 8},
-    {text: 'joyful', value: 6},
-    {text: 'sad', value: 3},
-    {text: 'exciting', value: 7},
-    {text: 'angry', value: 4},
-    {text: 'hopeful', value: 8},
-    {text: 'inspiring', value: 9, color: 'green'},
-    {text: 'dismal', value: 3},
-    {text: 'gloomy', value: 4},
-    {text: 'boring', value: 4},
-    {text: 'ordinary', value: 5},
-    {text: 'satisfied', value: 7},
-    {text: 'pleasing', value: 8},
+    { text: 'happy', value: 30 },
+    { text: 'joyful', value: 24 },
+    { text: 'sad', value: 12 },
+    { text: 'exciting', value: 28 },
+    { text: 'angry', value: 16 },
+    { text: 'hopeful', value: 32 },
+    { text: 'inspiring', value: 36, color: 'green' },
+    { text: 'dismal', value: 12 },
+    { text: 'gloomy', value: 16 },
+    { text: 'boring', value: 16 },
+    { text: 'ordinary', value: 20 },
+    { text: 'satisfied', value: 28 },
+    { text: 'pleasing', value: 32 },
     // Add more words as needed
   ];
 
@@ -40,15 +40,16 @@ const DataAnalysisWordCloud = () => {
         options={{
           words: barData.data,
           verticalEnabled: true,
-          minFont: 10,
-          maxFont: 60,
+          minFont: 40,
+          maxFont: 120,
           fontOffset: 5,
-          width: 400,
-          height: 500,
+          width: 600,
+          height: 700,
           fontFamily: 'Arial',
         }}
       />
     </View>
   );
 };
+
 export default DataAnalysisWordCloud;
