4d11356f8702a8a2ae2da876bbe66bc54ca81b96
michelle <michelle.vu@torontomu.ca>
2024-07-04 13:08:54 -0400

    Fixed WordCloud screen crashing
    WordCloud would crash when all word values were the same
    WordCloud would crash when video transcripts are empty

--- MyHealthMyRecord/screens/dataAnalysisWordCloud.tsx
+++ MyHealthMyRecord/screens/dataAnalysisWordCloud.tsx
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {useRoute} from '@react-navigation/native';
-import {Text, View} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {Alert, SafeAreaView, Text, View} from 'react-native';
 import WordCloud from 'rn-wordcloud';
 import {Dropdown} from 'react-native-element-dropdown';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const DataAnalysisWordCloud = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
   const route = useRoute();
   const barData = route.params?.data || {};
 
-  const [wordFrequency, setWordFrequency] = useState(barData.data || []);
   const [updatedData, setUpdatedData] = useState(barData.dataNoStop || []);
   const [dropdownValue, setDropdownValue] = useState(null);
 
@@ -55,69 +56,107 @@ const DataAnalysisWordCloud = () => {
     }));
   };
 
+  const validateData = data => {
+    const values = data.map(item => item.value);
+    const allSame = values.every(value => value === values[0]);
+
+    if (allSame) {
+      data[0].value += 1; // increase the value of the first word by 1
+    }
+
+    return data.map(item => ({
+      ...item,
+      value: isNaN(item.value)
+        ? Math.floor(Math.random() * 10) + 1
+        : item.value,
+      text: item.text || 'default',
+    }));
+  };
+
   useEffect(() => {
-    if (dropdownValue) {
-      let newPalette;
-      if (dropdownValue === 'IBM') {
-        newPalette = IBM_palette;
-      } else if (dropdownValue === 'Wong') {
-        newPalette = wong_palette;
-      } else if (dropdownValue === 'Tol') {
-        newPalette = tol_palette;
-      }
-      if (barData.dataNoStop) {
-        setUpdatedData(addPalette(barData.dataNoStop, newPalette));
+    if (!(barData.dataNoStop.length > 0)) {
+      Alert.alert(
+        'Cannot create word cloud',
+        'There are not enough words found in your videos to create a word cloud with. Try adding more videos with audio to your video set.',
+        [{text: 'OK', onPress: () => navigation.goBack()}],
+      );
+    } else {
+      const validatedData = validateData(barData.dataNoStop);
+      console.log('validatedData:', validatedData);
+      setUpdatedData(validatedData);
+
+      if (dropdownValue) {
+        console.log('dropdownValue true:', dropdownValue);
+        let newPalette;
+        if (dropdownValue === 'IBM') {
+          newPalette = IBM_palette;
+        } else if (dropdownValue === 'Wong') {
+          newPalette = wong_palette;
+        } else if (dropdownValue === 'Tol') {
+          newPalette = tol_palette;
+        }
+        if (barData.dataNoStop) {
+          setUpdatedData(addPalette(barData.dataNoStop, newPalette));
+        }
       }
     }
-  }, [dropdownValue]);
+  }, [dropdownValue, barData.dataNoStop]);
 
   return (
-    <View style={{flexDirection: 'column'}}>
-      <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <WordCloud
-          key={JSON.stringify(updatedData)}
-          options={{
-            words: updatedData,
-            verticalEnabled: true,
-            rotateRatio: 0.5,
-            minFont: 40,
-            maxFont: 120,
-            fontOffset: 5,
-            width: 700,
-            height: 800,
-            fontFamily: 'Arial',
-          }}
-        />
-      </View>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 20,
-          fontWeight: 'bold',
-          color: 'black',
-          paddingVertical: 10,
-        }}>
-        Select Color Palette:
-      </Text>
-      <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <Dropdown
-          data={dropdownData}
-          maxHeight={400}
-          style={{
-            height: 50,
-            width: 600,
-            paddingHorizontal: 20,
-            backgroundColor: '#DBDBDB',
-            borderRadius: 22,
-          }}
-          itemTextStyle={{textAlign: 'center'}}
-          labelField="label"
-          valueField="value"
-          value={dropdownValue}
-          onChange={item => setDropdownValue(item.value)}
-        />
-      </View>
-    </View>
+    <SafeAreaView>
+      {barData.dataNoStop.length > 0 ? (
+        <View>
+          <View style={{flexDirection: 'column'}}>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <WordCloud
+                key={JSON.stringify(updatedData)}
+                options={{
+                  words: updatedData,
+                  verticalEnabled: true,
+                  rotateRatio: 0.5,
+                  minFont: 40,
+                  maxFont: 120,
+                  fontOffset: 1,
+                  width: 700,
+                  height: 800,
+                  fontFamily: 'Arial',
+                }}
+              />
+            </View>
+            <Text
+              style={{
+                textAlign: 'center',
+                fontSize: 20,
+                fontWeight: 'bold',
+                color: 'black',
+                paddingVertical: 10,
+              }}>
+              Select Color Palette:
+            </Text>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Dropdown
+                data={dropdownData}
+                maxHeight={400}
+                style={{
+                  height: 50,
+                  width: 600,
+                  paddingHorizontal: 20,
+                  backgroundColor: '#DBDBDB',
+                  borderRadius: 22,
+                }}
+                itemTextStyle={{textAlign: 'center'}}
+                labelField="label"
+                valueField="value"
+                value={dropdownValue}
+                onChange={item => setDropdownValue(item.value)}
+              />
+            </View>
+          </View>
+        </View>
+      ) : (
+        <View></View>
+      )}
+    </SafeAreaView>
   );
 };
 
